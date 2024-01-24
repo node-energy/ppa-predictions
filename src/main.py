97fe1b1c123b6d1fcf74550f10ca7a2f623bed98
kaitimofejew <kai.timofejew@node.energy>
2024-01-24 12:08:40 +0100

    style: removed unnecessary code

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import inspect
 from fastapi import FastAPI
 from src.infrastructure.message_bus import MessageBus
 from src.infrastructure.unit_of_work import MemoryUnitOfWork
@@ -7,16 +6,6 @@ from src.api import components as components_api
 
 app = FastAPI()
 app.include_router(components_api.router)
-
-
-def inject_dependencies(handler, dependencies):
-    params = inspect.signature(handler).parameters
-    deps = {
-        name: dependency
-        for name, dependency in dependencies.items()
-        if name in params
-    }
-    return lambda message: handler(message, **deps)
 
 
 @app.on_event("startup")
