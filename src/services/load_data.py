bd6681cc5340daab1ed313a6cc855c3bcb66888d
Steffen Hauck <steffen.hauck@node.energy>
2024-07-02 08:52:51 +0200

    locally import MeteringOrMarketLocation as it required django setup

--- src/services/load_data.py
+++ src/services/load_data.py
@@ -8,17 +8,16 @@ from src.config import settings
 from src.utils.exceptions import NoMeteringOrMarketLocationFound, ConflictingEnergyData
 from src.utils.timezone import TIMEZONE_BERLIN
 
-from optinode.webserver.configurator.models import MeteringOrMarketLocation
 from optinode.webserver.configurator.enums import Measurand
 
 
 class AbstractLoadDataRetriever(abc.ABC):
-    def get_data(self, malo: str) -> pd.DataFrame:
+    def get_data(self, market_location_number: str) -> pd.Series:
         raise NotImplementedError()
 
 
 class APILoadDataRetriever(AbstractLoadDataRetriever):
-    def get_data(self, malo: str):
+    def get_data(self, market_location_number: str):
         pass
 
 
@@ -43,16 +42,18 @@ class OptinodeDataRetriever(AbstractLoadDataRetriever):  # TODO get rid of this
 
         django.setup()
 
-    def get_data(self, malo: str) -> pd.Series:
+    def get_data(self, market_location_number: str) -> pd.Series:
         start_date = dt.datetime.now(tz=TIMEZONE_BERLIN) - dt.timedelta(days=14)
-        malo = self._get_market_location(malo, start_date)
+        malo = self._get_market_location(market_location_number, start_date)
 
         energy_data: pd.Series = malo.get_load_profile(start=start_date, measurand=Measurand.POSITIVE)
         energy_data.name = "value"
         energy_data.index.name = "datetime"
         return energy_data
 
-    def _get_market_location(self, market_location_number: str, start_date: dt.datetime) -> MeteringOrMarketLocation:
+    def _get_market_location(self, market_location_number: str, start_date: dt.datetime):
+        from optinode.webserver.configurator.models import MeteringOrMarketLocation
+
         locations = MeteringOrMarketLocation.objects.filter(
             number=market_location_number,
             site__is_ppaaas=True
@@ -65,6 +66,8 @@ class OptinodeDataRetriever(AbstractLoadDataRetriever):  # TODO get rid of this
         return locations[0]
 
     @staticmethod
-    def _all_locations_have_equal_energy_data(locations: Iterable[MeteringOrMarketLocation], start_date: dt.datetime) -> bool:
+    def _all_locations_have_equal_energy_data(locations: Iterable, start_date: dt.datetime) -> bool:
+        if len(locations) == 1:
+            return True
         energy_data = [loc.get_load_profile(start=start_date, measurand=Measurand.POSITIVE) for loc in locations]
         return all([energy_data[0].equals(ed) for ed in energy_data[1:]])
