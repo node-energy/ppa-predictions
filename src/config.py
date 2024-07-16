c33495d7a20444e65bb400dd3c5f72286746c3ed
kaitimofejew <kai.timofejew@node.energy>
2024-07-16 13:27:22 +0200

    impr: Improved Readme and env var naming

--- src/config.py
+++ src/config.py
@@ -2,15 +2,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    debug: bool = False
+    debug: bool = True
     db_connection_string: str
     cors_origin: str = "https://localhost:3000"
     smtp_host: str = "smtp.office365.com"
     smtp_port: int = 587
     smtp_email: str = "kai.timofejew@node.energy"
     smtp_pass: str
-    recipient_consumption: str = "verbrauchsprognosen@ppa-mailbox.node.energy"
-    recipient_production: str = "erzeugungsprognosen@ppa-mailbox.node.energy"
+    mail_recipient_cons: str = "verbrauchsprognosen@ppa-mailbox.node.energy"
+    mail_recipient_prod: str = "erzeugungsprognosen@ppa-mailbox.node.energy"
     update_cron: str = "45 10 * * *"
     send_predictions_enabled: bool = False
     api_key: str = "node"
